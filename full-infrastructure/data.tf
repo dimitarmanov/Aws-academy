be7703679303e25cd0712c5e6d01371731a3b36d
Dma <molten988@gmail.com>
2023-06-07 21:34:09 +0300

    VPC-ALB-ASG-Cloudwatch-SNS

--- full-infrastructure/data.tf
+++ full-infrastructure/data.tf
@@ -1,25 +1,25 @@
 ## Getting latest Amazon Linux image ##
-#data "aws_ami" "latest_amazon_linux" {
-#  most_recent = true
-#  owners      = ["amazon"]
-#
-#  filter {
-#    name   = "name"
-#    values = [var.image_name]
-#  }
-#}
-#
-### Query VPC and subnets
-#data "aws_vpc" "main_vpc" {
-#  id = aws_vpc.main_vpc.id
-#}
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
 
-#data "aws_subnets" "default_vpc_subnets" {
-#  filter {
-#    name   = "vpc-id"
-#    values = [data.aws_vpc.main_vpc.id]
-#  }
-#}
+  filter {
+    name   = "name"
+    values = [var.image_name]
+  }
+}
+
+## Query VPC and subnets
+data "aws_vpc" "main_vpc" {
+  id = aws_vpc.main_vpc.id
+}
+
+data "aws_subnets" "default_vpc_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main_vpc.id]
+  }
+}
 
 ## Creating assume policy ##
 data "aws_iam_policy_document" "ec2_assume_role" {
@@ -40,7 +40,6 @@ data "aws_iam_policy" "ssm_managed" {
 }
 
 ## For SNS ##
-
 data "aws_iam_policy_document" "sns_policy" {
   policy_id = "sns-policy"
 
